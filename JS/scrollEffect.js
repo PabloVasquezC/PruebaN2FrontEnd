cd463803cadf80d3691942ff0e5ac4db44a837b2
Pablo V. from Nitro-5 <vascor.pablo@gmail.com>
2024-05-07 23:40:38 -0400

    fixing nav and adding scroll modification effect

---
+++ JS/scrollEffect.js
@@ -0,0 +1,19 @@
+window.addEventListener('scroll', function() {
+    const navigation = document.querySelector('nav');
+    const navLinks = document.querySelectorAll('.nav-link'); // Esto seleccionará todos los enlaces y el ícono
+    if (window.scrollY > 50) {
+        navigation.classList.add('nav-light');
+        navigation.classList.remove('nav-dark');
+        navLinks.forEach(link => {
+            link.classList.add('text-black'); // Asegúrate de definir esta clase en tu CSS
+            link.classList.remove('text-white');
+        });
+    } else {
+        navigation.classList.add('nav-dark');
+        navigation.classList.remove('nav-light');
+        navLinks.forEach(link => {
+            link.classList.remove('text-black');
+            link.classList.add('text-white');
+        });
+    }
+});
